406a2bfb63c72bcc25b59eaabcb09aee08a822c6
mygirok <forgirok@gmail.com>
2022-03-24 21:26:29 +0900

    Add player and monster struct

--- textRPG_v2/textRPG_v2.cpp
+++ textRPG_v2/textRPG_v2.cpp
@@ -13,6 +13,102 @@ enum MAIN_MENU
 	MM_EXIT
 };
 
+enum MAP_TYPE
+{
+	MT_NONE,
+	MT_EASY,
+	MT_NORMAL,
+	MT_HARD,
+	MT_BACK
+};
+
+enum JOB
+{
+	JOB_NONE,
+	JOB_KNIGHT,
+	JOB_ARCHER,
+	JOB_WIZARD,
+	JOB_END
+};
+
+enum ITEM_TYPE
+{
+	IT_NONE,
+	IT_WEAPON,
+	IT_ARMOR,
+	IT_BACK
+};
+
+enum EQUIP
+{
+	EQ_WEAPON,
+	EQ_ARMOR,
+	EQ_MAX
+};
+
+#define NAME_SIZE	32
+#define ITEM_DESC_LENGTH	512
+#define INVENTORY_MAX		20
+#define STORE_WEAPON_MAX	3
+#define STORE_ARMOR_MAX		3
+#define LEVEL_MAX			10
+
+struct _tagItem
+{
+	char	strName[NAME_SIZE];
+	char	strTypeName[NAME_SIZE];
+	ITEM_TYPE	eType;
+	int		iMin;
+	int		iMax;
+	int		iPrice;
+	int		iSell;
+	char	strDesc[ITEM_DESC_LENGTH];
+};
+
+struct _tagInventory
+{
+	_tagItem	tItem[INVENTORY_MAX];
+	int			iItemCount;
+	int			iGold;
+};
+
+struct _tagPlayer
+{
+	char	strName[NAME_SIZE];
+	char	strJobName[NAME_SIZE];
+	JOB		eJob;
+	int		iAttackMin;
+	int		iAttackMax;
+	int		iArmorMin;
+	int		iArmorMax;
+	int		iHP;
+	int		iHPMax;
+	int		iMP;
+	int		iMPMax;
+	int		iExp;
+	int		iLevel;
+	_tagItem	tEquip[EQ_MAX];
+	bool		bEquip[EQ_MAX];
+	_tagInventory	tInventory;
+};
+
+struct _tagMonster
+{
+	char	strName[NAME_SIZE];
+	int		iAttackMin;
+	int		iAttackMax;
+	int		iArmorMin;
+	int		iArmorMax;
+	int		iHP;
+	int		iHPMax;
+	int		iMP;
+	int		iMPMax;
+	int		iLevel;
+	int		iExp;
+	int		iGoldMin;
+	int		iGoldMax;
+};
+
 // Check input number
 // return input if input is right
 // return INT_MAX if input is wrong number
@@ -47,17 +143,58 @@ int OutputMainMenu()
 
 	return iMenu;
 }
+
+int OutputMapMenu()
+{
+	system("cls");
+
+	cout << "********************** Map ***********************" << endl;
+	cout << "1. Easy" << endl;
+	cout << "2. Normal" << endl;
+	cout << "3. Hard" << endl;
+	cout << "4. Back" << endl;
+	cout << "Choose Map : ";
+	int iMenu = InputInt();
+
+	if (iMenu == INT_MAX || iMenu <= MT_NONE || iMenu > MT_BACK)
+		return MT_NONE;
+
+	return iMenu;
+}
+
+// Handles map loops
+void RunMap()
+{
+	bool bLoop = true;
+	while (bLoop)
+	{
+		switch (OutputMapMenu())
+		{
+		case MT_EASY:
+			break;
+		case MT_NORMAL:
+			break;
+		case MT_HARD:
+			break;
+		case MT_BACK:
+			bLoop = false;
+			break;
+		}
+	}
+}
+
 int main()
 {
 	srand((unsigned int)time(0));
 
 	bool bLoop = true;
 
-	while (bLoop )
+	while (bLoop)
 	{
 		switch (OutputMainMenu())
 		{
 		case MM_MAP:
+			RunMap();
 			break;
 		case MM_STORE:
 			break;
